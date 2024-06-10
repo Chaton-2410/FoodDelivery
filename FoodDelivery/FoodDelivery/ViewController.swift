728443f4158c31fc665cc632395c89f4e5eea521
Valeriya Trofimova <chaton-2410@mail.ru>
2024-06-10 21:17:55 +0300

    Created base project, removed storyboard reference

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Valeriya Trofimova on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .green
+    }
+}
+
