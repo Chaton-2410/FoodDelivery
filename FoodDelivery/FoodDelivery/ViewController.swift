92491d44ef6face49f43ae69fba1d6d2b3800ddd
Valeriya Trofimova <chaton-2410@mail.ru>
2024-06-12 14:37:46 +0300

    Added new Roboto fonts

--- FoodDelivery/FoodDelivery/ViewController.swift
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let view = UILabel()
+        view.textColor = .white
+        view.text = "Hello World!"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = .Roboto.italic.size(of: 14)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        view.backgroundColor = .green
+        view.backgroundColor = .red
+        view.addSubview(label)
+        setConstaints()
+    }
+    
+    private func setConstaints() {
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
 }
 
